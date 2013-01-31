5ef722479b0c6acfad327dcfa16522590f166d2c
Axel Tetzlaff <axel.tetzlaff@gmx.de>
2013-01-31 10:45:41 +0000

    added various umlauts

--- lib/unlaut.rb
+++ lib/unlaut.rb
@@ -3,13 +3,17 @@ module Unlaut
   class Map
     class << self
       def replace(from, to)
-        @map ||= {}
-        @map[from] = to
-        @src_chars = nil
+        if from.length > 1
+          from.each_char{|c| replace c, to}
+        else
+          @map ||= {}
+          @map[from] = to
+          @src_chars = nil
+        end
       end
 
       def src_chars
-        @src_chars ||= @map.keys.join()
+        @src_chars ||= Regexp.new('[' + @map.keys.join()  + ']')
       end
       
       def execute(string)
@@ -17,7 +21,24 @@ module Unlaut
       end
     end
 
-    replace 'ä', 'a'
+    replace 'ÂÃÄÀÁÅÆ', 'A'
+    replace 'Ç', 'C'
+    replace 'ÈÉÊË', 'E'
+    replace 'ÌÍÎÏ', 'I'
+    replace 'Ð', 'D'
+    replace 'Ñ', 'N'
+    replace 'ÒÓÔÕÖØ', 'O'
+    replace 'ÙÚÛÜ', 'U'
+    replace 'ÞÝ', 'Y'
+    replace 'ÙÚÛÜÝ', 'U'
+    replace 'ß', 'ss'
+    replace 'àáâãäåæ', 'a'
+    replace 'ç', 'c'
+    replace 'èéêë', 'e'
+    replace 'ìíî', 'i'
+    replace 'ðñòóôõöø', 'o'
+    replace 'ùúûü', 'u'
+    replace 'ýþÿ', 'y'
   end
 end
  
---
+++ test/unlaut_test.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+require 'unlaut'
+require "test/unit"
+ 
+class TestUnlaut < Test::Unit::TestCase
+  def test_basics
+    assert_equal 'Gruss Gott, das musste klappen',  'Grüß Gott, das müsste klappen'.to_i18n
+  end
+end
